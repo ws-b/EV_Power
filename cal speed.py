ee2eb77696d4ffd35f80a59fdf79e3ce72884e7f
WSONG <mu4215@gmail.com>
2023-04-22 21:14:13 +0900

    UTM -> WGS84 속도/가속도 계산 [가속도 오류 수정완료]

--- cal speed.py
+++ cal speed.py
@@ -8,8 +8,8 @@ mac_folder_path = '/Users/woojin/Downloads/경로데이터 ᄉ
 mac_save_path = '/Users/woojin/Downloads/경로데이터 샘플 및 데이터 정의서/포인트 경로 데이터 속도:가속도 처리/'
 win_folder_path = 'D:\\Data\\대학교 자료\\켄텍 자료\\삼성미래과제\\경로데이터 샘플 및 데이터 정의서\\포인트 경로 데이터 Processed\\'
 win_save_path = 'D:\\Data\\대학교 자료\\켄텍 자료\\삼성미래과제\\경로데이터 샘플 및 데이터 정의서\\포인트 경로 데이터 속도-가속도 처리\\'
-folder_path = mac_folder_path
-save_path = mac_save_path
+folder_path = win_folder_path
+save_path = win_save_path
 # get a list of all files in the folder with the .csv extension
 file_lists = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f)) and f.endswith('.csv')]
 #file_list = '164_2.csv'
@@ -24,22 +24,21 @@ for file_list in file_lists:
     latitudes = data[:, 1]
     longitudes = data[:, 2]
 
-    # convert the latitude and longitude data to UTM coordinates
-    lat = []
-    log = []
-    for i in range (0, len(latitudes)):
-        transformer = Transformer.from_crs("epsg:5179", "epsg:4326")
-        utm_lat, utm_log = transformer.transform(latitudes[i], longitudes[i])
-        # UTM-K(Bassel) 도로명주소 지도 사용 중 to Wgs84 경도/위도, GPS사용 전지구 좌표
-        lat.append(utm_lat)
-        log.append(utm_log)
+    # Convert UTM coordinates to WGS84 coordinates
+    wgs84_latitudes = []
+    wgs84_longitudes = []
+    transformer = Transformer.from_crs("epsg:5179", "epsg:4326")
+    for i in range(len(latitudes)):
+        wgs84_lat, wgs84_long = transformer.transform(latitudes[i], longitudes[i])
+        wgs84_latitudes.append(wgs84_lat)
+        wgs84_longitudes.append(wgs84_long)
 
-    # Calculate distance between each pair of consecutive UTM coordinates
-    speeds = [distance.distance(Point(lat[i], log[i]), Point(lat[i+1], log[i+1])).m / (times[i+1]-times[i]) for i in range(0,len(lat)-1)]
+    # Calculate distance between each pair of consecutive WGS84 coordinates
+    speeds = [distance.distance(Point(wgs84_latitudes[i], wgs84_longitudes[i]), Point(wgs84_latitudes[i + 1], wgs84_longitudes[i + 1])).m / (times[i + 1] - times[i]) for i in range(0, len(wgs84_latitudes) - 1)]
     speeds.append(speeds[-1])
 
     # Calculate the acceleration by taking the derivative of the speeds with respect to time
-    accelerations = [((speeds[i+1] - speeds[i]) / (times[i+1] - times[i])) for i in range(len(speeds)-1)]
+    accelerations = [((speeds[i + 1] - speeds[i]) / (times[i + 1] - times[i])) for i in range(len(speeds) - 1)]
     accelerations.append(accelerations[-1])
 
     # Use numpy.column_stack() to append the list as a new column
