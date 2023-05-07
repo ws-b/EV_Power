4b7ed7b02a1ab60b393333576f037548a0d404cd
WSONG <wsong@kentech.ac.kr>
2023-05-07 17:56:49 +0900

    파일명 수정, get_file_list 함수화로 빼냈음

--- Merge_Power.py
+++ Merge_Power.py
@@ -1,19 +1,12 @@
 import os
 import numpy as np
+from make_array import get_file_list
 
 # 파일이 들어있는 폴더 경로
 win_folder_path = 'D:\\Data\\대학교 자료\\켄텍 자료\\삼성미래과제\\경로데이터 샘플 및 데이터 정의서\\포인트 경로 데이터 속도-가속도 처리\\'
 mac_folder_path = '/Users/woojin/Downloads/경로데이터 샘플 및 데이터 정의서/포인트 경로 데이터 속도-가속도 처리/'
 folder_path = win_folder_path
 
-def get_file_list(folder_path):
-    # 폴더 내의 모든 파일 리스트 가져오기
-    file_list = os.listdir(folder_path)
-    txt_files = []
-    for file in file_list:
-        if file.endswith('.csv'):
-            txt_files.append(file)
-    return txt_files
 
 # 파일 리스트 가져오기
 files = get_file_list(folder_path)
--- total_distance_triptime.py
+++ total_distance_triptime.py
@@ -1,14 +1,7 @@
 import os
 import numpy as np
+from make_array import get_file_list
 
-def get_file_list(folder_path):
-    # 폴더 내의 모든 파일 리스트 가져오기
-    file_list = os.listdir(folder_path)
-    txt_files = []
-    for file in file_list:
-        if file.endswith('.csv'):
-            txt_files.append(file)
-    return txt_files
 
 # 파일이 들어있는 폴더 경로
 win_folder_path = 'D:\\Data\\대학교 자료\\켄텍 자료\\삼성미래과제\\경로데이터 샘플 및 데이터 정의서\\포인트 경로 데이터 속도-가속도 처리\\'
