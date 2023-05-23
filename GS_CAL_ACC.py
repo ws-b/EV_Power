66a02df06480ce4d0076e8ab139d28cb98604e80
WSONG <mu4215@gmail.com>
2023-05-23 16:18:10 +0900

    Minor fix

--- GS_CAL_ACC.py
+++ GS_CAL_ACC.py
@@ -10,25 +10,25 @@ win_save_path = 'D:\\Data\\대학교 자료\\켄텍 자료\\삼성미래과제\\
 folder_path = win_folder_path
 save_path = win_save_path
 # get a list of all files in the folder with the .csv extension
-# file_lists = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f)) and f.endswith('.csv')]
-# for file_list in file_lists:
-#     file = open(folder_path + file_list, "r")
+file_lists = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f)) and f.endswith('.csv')]
+for file_list in file_lists:
+    file = open(folder_path + file_list, "r")
 #   file = '/Users/woojin/Downloads/한국에너지공과대학교_샘플데이터/processed/bms.01241228177.02_parsed.csv'
-    file = 'D:\\Data\\대학교 자료\\켄텍 자료\\삼성미래과제\\한국에너지공과대학교_샘플데이터\\processed\\bms.01241228177.02_parsed.csv'
+#   file = 'D:\\Data\\대학교 자료\\켄텍 자료\\삼성미래과제\\한국에너지공과대학교_샘플데이터\\processed\\bms.01241228177.02_parsed.csv'
     # Load CSV file into a pandas DataFrame
-    df = pd.read_csv(file)
+    df = pd.read_csv(file, dtype={'device_no': str, 'measured_month': str})
 
     # 속도 단위를 km/h에서 m/s로 변환
     df['emobility_spd_m_per_s'] = df['emobility_spd'] * 0.27778
 
     # 시간과 변환된 속도의 변화를 계산합니다.
     df['time'] = df['time'].str.strip()
-    df['time'] = pd.to_datetime(df['time'], format='%Y-%m-%d %H:%M:%S')
+    df['time'] = pd.to_datetime(df['time'], format='%y-%m-%d %H:%M:%S')
     df['time_diff'] = df['time'].diff().dt.total_seconds()
     df['emobility_spd_m_per_s'] = df['emobility_spd'] * 0.27778
     df['spd_diff'] = df['emobility_spd_m_per_s'].diff()
 
-# 가속도 계산
+    # 가속도 계산
     df['acceleration'] = df['spd_diff'] / df['time_diff']
 
     # NaN 값을 0으로 바꿔주거나, 원하는 값으로 채워줍니다.
@@ -36,5 +36,5 @@ save_path = win_save_path
 
     data_save = df[['time', 'emobility_spd_m_per_s', 'acceleration', 'trip_chrg_pw', 'trip_dischrg_pw', 'soc', 'soh']].copy()
 
-# csv 파일로 저장하기
-    data_save.to_csv(f"{save_path}{str(device_no.iloc[0])}.csv", index=False)
+    # csv 파일로 저장하기
+    data_save.to_csv(f"{save_path}{df['device_no'].iloc[0].replace(' ', '')}{'-0' + df['measured_month'].iloc[0][-2:].replace(' ', '')}.csv", index=False)
