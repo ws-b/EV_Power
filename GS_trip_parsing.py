0232c0077568516335aebfa87639a6c5e9bdf840
WSONG <mu4215@gmail.com>
2023-05-24 12:12:35 +0900

    초기화 cable connection 상태로 parsing 진행해야함.

--- GS_trip_parsing.py
+++ GS_trip_parsing.py
@@ -30,26 +30,3 @@ files.sort()
 #for file in files:
     # csv 파일 불러오기
 #    df = pd.read_csv(folder_path + file, sep=',')
-    file = 'D:\\Data\\대학교 자료\\켄텍 자료\\삼성미래과제\\한국에너지공과대학교_샘플데이터\\speed-acc\\01241228177-02.csv'
-    df = pd.read_csv(file, sep=',')
-    df['time'] = pd.to_datetime(df['time'])
-
-    # pandas DataFrame을 numpy 배열로 변환
-    data = df.values
-
-    # DATETIME을 초로 변환
-    time_in_seconds = (data[:, 0] - np.datetime64('1970-01-01T00:00:00Z')) / np.timedelta64(1, 's')
-
-    # Speed가 0인 기간이 300초 이상인 경우, 혹은 chrg_cable_connect가 1인 경우를 찾기
-    split_conditions = np.logical_or((data[:, 1] == 0) & (np.diff(time_in_seconds, prepend=time_in_seconds[0]) >= 300), data[:, 5] == 1)
-
-    # 위 조건에 해당하는 지점에서 데이터 분할
-    split_indices = np.where(split_conditions)[0]
-    split_indices = np.append(split_indices, len(data))  # 마지막 인덱스 추가
-"""
-    # 각각의 데이터 조각을 별도의 csv 파일로 저장
-    for i in range(len(split_indices) - 1):
-        split_data = data[split_indices[i]:split_indices[i + 1]]
-        pd.DataFrame(split_data, columns=df.columns).to_csv(f'output_{i}.csv', index=False)
-        
-"""
