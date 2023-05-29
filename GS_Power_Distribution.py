5e9004d4726f4dc758611651830b4161dab1878e
WSONG <mu4215@gmail.com>
2023-05-29 13:39:39 +0900

    Minor FIX

--- GS_Power_Distribution.py
+++ GS_Power_Distribution.py
@@ -1,8 +1,8 @@
+import os
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
-import os
 
 # 파일이 들어있는 폴더 경로
 win_folder_path = 'D:\\Data\\대학교 자료\\켄텍 자료\\삼성미래과제\\한국에너지공과대학교_샘플데이터\\kona_ev\\'
@@ -15,6 +15,7 @@ file_lists = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(
 file_lists.sort()
 
 all_distance_per_total_power = []
+over_30_files = []
 
 for file_list in file_lists:
     # 파일 경로 생성하기
@@ -41,6 +42,10 @@ for file_list in file_lists:
     # 모든 파일의 distance_per_total_power 값 모으기
     all_distance_per_total_power.append(distance_per_total_power_km_kWh)
 
+    # 만약 distance_per_total_power_km_kWh가 50 이상이면, 해당 파일 이름을 over_50_files에 추가
+    if distance_per_total_power_km_kWh >= 30:
+        over_30_files.append(file_list)
+
 # 전체 파일에 대한 히스토그램 그리기
 hist_data = sns.histplot(all_distance_per_total_power, bins='auto', color='gray', kde=False)
 
@@ -58,3 +63,8 @@ plt.ylabel('Number of trips')
 plt.title('Total Distance / Total Power Distribution')
 plt.grid(False)
 plt.show()
+
+# 50 이상의 비율을 가진 파일들 출력
+print("Files with a ratio of Total Distance / Total Power greater than 50:")
+for file in over_30_files:
+    print(file)
