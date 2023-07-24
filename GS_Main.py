0d4f93ace6349fd2f618fb67937b5b4bb2c5a510
ws-b <mu4215@gmail.com>
2023-07-24 10:47:56 +0900

    Create GS Main and others

---
+++ GS_Main.py
@@ -0,0 +1,48 @@
+import os
+import csv
+import pandas as pd
+import numpy as np
+from datetime import datetime
+from tqdm import tqdm
+from GS_preprocessing_1 import get_file_list, parse_spacebar
+from GS_preprocessing_2 import process_files_combined
+from GS_preprocessing_3 import process_files_trip_by_trip
+
+def pre_process():
+    while True:
+        print("1: Initial GS Data Parsing")
+        print("2: Calculating Speed(m/s), Acceleration(m/s^2)")
+        print("3: Trip by Trip Parsing")
+        choice = int(input("Enter number you want to run: "))
+
+        if choice == 1:
+            folder_path = os.path.normpath('D:\\Data\\대학교 자료\\켄텍 자료\\삼성미래과제\\한국에너지공과대학교_샘플데이터')
+            save_path = os.path.normpath('D:\\Data\\대학교 자료\\켄텍 자료\\삼성미래과제\\한국에너지공과대학교_샘플데이터\\processed')
+            file_lists = get_file_list(folder_path)
+            parse_spacebar(file_lists, folder_path, save_path)
+            break
+
+        elif choice == 2:
+            folder_path = os.path.normpath('D:\Data\대학교 자료\켄텍 자료\삼성미래과제\한국에너지공과대학교_샘플데이터\processed')
+            save_path = os.path.normpath('D:\Data\대학교 자료\켄텍 자료\삼성미래과제\한국에너지공과대학교_샘플데이터\speed-acc')
+            file_list = get_file_list(folder_path)
+            process_files_combined(file_list, folder_path, save_path)
+            break
+
+        elif choice == 3:
+            folder_path = os.path.normpath('D:\\Data\\대학교 자료\\켄텍 자료\\삼성미래과제\\한국에너지공과대학교_샘플데이터\\speed-acc')
+            save_path = os.path.normpath('D:\\Data\\대학교 자료\\켄텍 자료\\삼성미래과제\\한국에너지공과대학교_샘플데이터\\trip_by_trip')
+            file_list = get_file_list(folder_path)
+            process_files_trip_by_trip(file_list, folder_path, save_path)
+            break
+
+        elif choice == 4:
+            print("Quitting the program.")
+            break
+
+        else:
+            print("Invalid choice. Please try again.")
+
+
+if __name__ == "__main__":
+    pre_process()
---
+++ GS_preprocessing_2.py
@@ -0,0 +1,52 @@
+def process_files_combined(file_lists, folder_path, save_path):
+    for file in tqdm(file_lists):
+        file_path = os.path.join(folder_path, file)
+
+        # Load CSV file into a pandas DataFrame
+        df = pd.read_csv(file_path, dtype={'device_no': str, 'measured_month': str})
+
+        # reverse the DataFrame based on the index
+        df = df[::-1]
+
+        # calculate time and speed changes
+        df['time'] = df['time'].str.strip()
+        df['time'] = pd.to_datetime(df['time'], format='%y-%m-%d %H:%M:%S')
+        t = pd.to_datetime(df['time'], format='%Y-%m-%d %H:%M:%S')
+        t_diff = t.diff().dt.total_seconds()
+        df['time_diff'] = t_diff
+        df['speed'] = df['emobility_spd'] * 0.27778
+        df['spd_diff'] = df['speed'].diff()
+
+        # calculate acceleration
+        df['acceleration'] = df['spd_diff'] / df['time_diff']
+
+        # replace NaN values with 0 or fill with desired values
+        df['acceleration'] = df['acceleration'].replace(np.nan, 0)
+
+        # merge selected columns into a single DataFrame
+        df['Power_IV'] = df['pack_volt'] * df['pack_current']
+
+        # Calculate time difference in seconds
+        Power = df['Power_IV'].tolist()
+
+        # Convert lists to numpy arrays for vectorized operations
+        Power = np.array(Power)
+        t_diff = np.array(t_diff.fillna(0))
+
+        # Calculate energy by multiplying power with time difference
+        Energy = Power * t_diff / 3600 / 1000
+
+        # Convert the energy back to a list and add it to the DataFrame
+        df['Energy_IV'] = Energy.tolist()
+
+        # merge selected columns into a single DataFrame
+        data_save = df[['time', 'speed', 'acceleration', 'trip_chrg_pw', 'trip_dischrg_pw', 'pack_current', 'pack_volt',
+                        'chrg_cable_conn', 'ext_temp', 'int_temp', 'soc', 'soh', 'cell_volt_list', 'Power_IV',
+                        'Energy_IV']].copy()
+
+        # save as a CSV file
+        data_save.to_csv(os.path.join(save_path,
+                                      f"{df['device_no'].iloc[0].replace(' ', '')}{'-0' + df['measured_month'].iloc[0][-2:].replace(' ', '')}.csv"),
+                         index=False)
+
+    print('Done')
