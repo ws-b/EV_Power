b9777238d06be0c6eca3feadc63e92d60aa47b0b
ws-b <mu4215@gmail.com>
2023-07-24 13:40:43 +0900

    Update GS_Parsing_Main.py

--- GS_Parsing_Main.py
+++ GS_Parsing_Main.py
@@ -2,17 +2,19 @@ import os
 import csv
 import pandas as pd
 import numpy as np
-from datetime import datetime
 from tqdm import tqdm
 from GS_preprocessing_1 import get_file_list, parse_spacebar
-from GS_preprocessing_2 import parse_spacebar
-from GS_preprocessing_3 import process_files_combined
+from GS_preprocessing_2 import process_files_combined
+from GS_preprocessing_3 import process_files_trip_by_trip
+from GS_filtering_data import move_files
 
 def pre_process():
     while True:
         print("1: Initial GS Data Parsing")
         print("2: Calculating Speed(m/s), Acceleration(m/s^2)")
         print("3: Trip by Trip Parsing")
+        print("4: Filtering Data")
+        print("5: Quitting the program.")
         choice = int(input("Enter number you want to run: "))
 
         if choice == 1:
@@ -36,9 +38,18 @@ def pre_process():
             process_files_trip_by_trip(file_list, folder_path, save_path)
             break
 
-        elif choice == 4:
-            print("Quitting the program.")
+        elif choice == 4: ### After Merge Power
+            folder_path = os.path.normpath('D:\\Data\\대학교 자료\\켄텍 자료\\삼성미래과제\\한국에너지공과대학교_샘플데이터\\ioniq5')
+            moved_path = os.path.join(folder_path, 'moved')
+            if not os.path.exists(moved_path):
+                os.makedirs(moved_path)
+            file_list = get_file_list(folder_path)
+            move_files(file_list, folder_path, moved_path)
             break
+
+        elif choice == 5:
+            print("Quitting the program.")
+            return
 
         else:
             print("Invalid choice. Please try again.")
@@ -46,4 +57,3 @@ def pre_process():
 
 if __name__ == "__main__":
     pre_process()
-
