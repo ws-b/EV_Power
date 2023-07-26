3d97362dd55f5b596b92f3944493e1759534f3c5
ws-b <mu4215@gmail.com>
2023-07-26 13:28:10 +0900

    Update GS_plot_energy_distribution.py

--- GS_plot_energy_distribution.py
+++ GS_plot_energy_distribution.py
@@ -13,25 +13,24 @@ def plot_model_energy_dis(file_lists, folder_path):
         file_path = os.path.join(folder_path, file)
         data = pd.read_csv(file_path)
 
-        # extract time, speed, Energy
         t = pd.to_datetime(data['time'], format='%Y-%m-%d %H:%M:%S')
+        t_diff = t.diff().dt.total_seconds().fillna(0)
+        t_diff = np.array(t_diff.fillna(0))
+
         v = data['speed']
-        Power = data['Power']
+        model_power = data['Power']
+        model_power = np.array(model_power)
+        model_energy = model_power * t_diff / 3600 / 1000
+        model_energy_cumulative = model_energy.cumsum()
 
         # calculate total distance considering the sampling interval (2 seconds)
         total_distance = np.sum(v * 2)
 
-        # total Energy sum
-        total_energy = np.sum(Energy)
-
-        # calculate total time sum
-        total_time = np.sum(t.diff().dt.total_seconds())
-
         # calculate Total distance / Total Energy for each file (if Total Energy is 0, set the value to 0)
-        distance_per_total_energy_km_kWh = (total_distance / 1000) / (total_energy) if total_energy != 0 else 0
+        distance_per_total_energy = (total_distance / 1000) / (model_energy_cumulative[-1]) if model_energy_cumulative[-1] != 0 else 0
 
         # collect all distance_per_total_Energy values for all files
-        all_distance_per_total_energy.append(distance_per_total_energy_km_kWh)
+        all_distance_per_total_energy.append(distance_per_total_energy)
 
     # plot histogram for all files
     hist_data = sns.histplot(all_distance_per_total_energy, bins='auto', color='gray', kde=False)
@@ -50,9 +49,9 @@ def plot_model_energy_dis(file_lists, folder_path):
 
     # set x-axis range (from 0 to 25)
     plt.xlim(0, 25)
-    plt.xlabel('Total Distance / Total Energy (km/kWh)')
+    plt.xlabel('Total Distance / Total Model Energy (km/kWh)')
     plt.ylabel('Number of trips')
-    plt.title('Total Distance / Total Energy Distribution')
+    plt.title('Total Distance / Total Model Energy Distribution')
     plt.grid(False)
     plt.show()
 def plot_bms_energy_dis(file_lists, folder_path):
@@ -63,27 +62,24 @@ def plot_bms_energy_dis(file_lists, folder_path):
         file_path = os.path.join(folder_path, file)
         data = pd.read_csv(file_path)
 
-        # extract time, speed, CHARGE, DISCHARGE
         t = pd.to_datetime(data['time'], format='%Y-%m-%d %H:%M:%S')
-        v = data['emobility_spd_m_per_s'].tolist()
-        CHARGE = data['trip_chrg_pw'].tolist()
-        DISCHARGE = data['trip_dischrg_pw'].tolist()
+        t_diff = t.diff().dt.total_seconds().fillna(0)
+        t_diff = np.array(t_diff.fillna(0))
+
+        v = data['speed'].tolist()
+        bms_power = data['Power_IV']
+        bms_power = np.array(bms_power)
+        data_energy = bms_power * t_diff / 3600 / 1000
+        data_energy_cumulative = data_energy.cumsum()
 
         # calculate total distance considering the sampling interval (2 seconds)
         total_distance = np.sum(v * 2)
 
-        # calculate net_discharge by subtracting CHARGE sum from DISCHARGE sum
-        net_discharge = 0
-        for i in range(len(DISCHARGE) - 1, -1, -1):
-            net_discharge = DISCHARGE[i] - CHARGE[i]
-            if net_discharge != 0:
-                break
-
         # calculate Total distance / net_discharge for each file (if net_discharge is 0, set the value to 0)
-        distance_per_total_energy_km_kWh = (total_distance / 1000) / net_discharge if net_discharge != 0 else 0
+        distance_per_total_energy = (total_distance / 1000) / data_energy_cumulative[-1] if data_energy_cumulative[-1] != 0 else 0
 
         # collect all distance_per_total_energy values for all files
-        all_distance_per_total_energy.append(distance_per_total_energy_km_kWh)
+        all_distance_per_total_energy.append(distance_per_total_energy)
 
     # plot histogram for all files
     hist_data = sns.histplot(all_distance_per_total_energy, bins='auto', color='gray', kde=False)
@@ -102,8 +98,8 @@ def plot_bms_energy_dis(file_lists, folder_path):
 
     # set x-axis range (from 0 to 25)
     plt.xlim(0, 25)
-    plt.xlabel('Total Distance / Net Discharge (km/kWh)')
+    plt.xlabel('Total Distance / Total BMS Energy (km/kWh)')
     plt.ylabel('Number of trips')
-    plt.title('Total Distance / Net Discharge Distribution')
+    plt.title('Total Distance / Total BMS Energy Distribution')
     plt.grid(False)
     plt.show()
