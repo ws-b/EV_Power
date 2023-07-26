766437f6fb870d74a1cfc583c7ed7449f93ebdff
ws-b <mu4215@gmail.com>
2023-07-26 13:10:07 +0900

    Update GS_filtering_data.py

--- GS_filtering_data.py
+++ GS_filtering_data.py
@@ -9,29 +9,33 @@ def move_files(file_lists, folder_path, moved_path):
         file_path = os.path.join(folder_path, file)
         data = pd.read_csv(file_path)
 
-        # extract time, Power, CHARGE, DISCHARGE
-        t = pd.to_datetime(data['time'], format='%Y-%m-%d %H:%M:%S')
+        # extract time, Energy, CHARGE, DISCHARGE
         v = data['speed']
-        CHARGE = data['trip_chrg_pw'].tolist()
-        DISCHARGE = data['trip_dischrg_pw'].tolist()
-
-        Energy_IV = data['Energy_IV'].tolist()
+        bms_power = data['Power_IV']
+        t = pd.to_datetime(data['time'], format='%Y-%m-%d %H:%M:%S')
+        t_diff = t.diff().dt.total_seconds().fillna(0)
+        t_diff = np.array(t_diff.fillna(0))
+        bms_power = np.array(bms_power)
+        data_energy = bms_power * t_diff / 3600 / 1000
+        data_energy_cumulative = data_energy.cumsum()
 
         # calculate total distance considering the sampling interval (2 seconds)
-        total_distance = np.sum(v * 2)
+        v = np.array(v)
+        distance = v * t_diff
+        total_distance = distance.cumsum()[-1]
 
-        Energy_VI_cumulative = np.cumsum(Energy_IV).tolist()
-
-        # calculate difference between CHARGE and DISCHARGE
-        net_charge = np.array(DISCHARGE) - np.array(CHARGE)
-
-        # convert Power data to kWh and perform cumulative calculation
-        Energy_kWh = data['Energy']  # convert kW to kWh considering the 2-second time interval
-        Energy_kWh_cumulative = Energy_kWh.cumsum().tolist()
+        # convert power data to kWh and perform cumulative calculation
+        model_power = data['Power']
+        model_power = np.array(model_power)
+        model_energy = model_power * t_diff / 3600 / 1000
+        model_energy_cumulative = model_energy.cumsum()
 
         # move file if the time range is more than 5 minutes and total distance is less than 1000 and Power_kWh_cumulative is less than 0
         time_range = t.iloc[-1] - t.iloc[0]
-        if time_range.total_seconds() < 300  or total_distance < 1000 or Energy_kWh_cumulative[-1] < 0 or Energy_VI_cumulative[-1] < 0:    # 5 minutes = 300 seconds
+        time_limit = 300
+        distance_limit = 1000
+        Energy_limit = 0
+        if time_range.total_seconds() < time_limit or total_distance < distance_limit or model_energy_cumulative[-1] < Energy_limit or  data_energy_cumulative[-1] < Energy_limit:    # 5 minutes = 300 seconds
             os.replace(file_path, os.path.join(moved_path, file))
 
     print("Done!")
