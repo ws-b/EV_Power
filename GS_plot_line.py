62f9b2d48bcdd93b04d1c2609cefca58a5880858
ws-b <mu4215@gmail.com>
2023-07-28 17:03:23 +0900

    Update GS_plot_line.py

--- GS_plot_line.py
+++ GS_plot_line.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from tqdm import tqdm
-
+import seaborn as sns
 def plot_stacked_graph(file_lists, folder_path):
     for file in tqdm(file_lists[31:35]):
         file_path = os.path.join(folder_path, file)
@@ -27,7 +27,6 @@ def plot_stacked_graph(file_lists, folder_path):
         plt.legend(loc='upper left')
 
         plt.show()
-
 def plot_model_energy(file_lists, folder_path):
     for file in tqdm(file_lists[31:35]):
         file_path = os.path.join(folder_path, file)
@@ -130,9 +129,8 @@ def plot_energy_comparison(file_lists, folder_path):
         plt.title('Model Energy vs. BMS Energy')
         plt.tight_layout()
         plt.show()
-
 def plot_regen_brake_effect(file_lists, folder_path):
-    for file in tqdm(file_lists[30:35]):
+    for file in tqdm(file_lists[31:35]):
         file_path = os.path.join(folder_path, file)
         data = pd.read_csv(file_path)
 
@@ -160,6 +158,12 @@ def plot_regen_brake_effect(file_lists, folder_path):
         data_energy_masked = data_energy[mask]
         acceleration_masked = acceleration[mask]
 
+        total_drive_time = t_min.iloc[-1] - t_min.iloc[0]
+        regen_brake_time = t_min_masked.iloc[-1] - t_min_masked.iloc[0]
+        regen_brake_ratio = regen_brake_time / total_drive_time
+
+        print(f"Regenerative braking time 비율은{regen_brake_ratio}")
+
         # Plot the comparison graph
         plt.figure(figsize=(10, 6))  # Set the size of the graph
         ax1 = plt.gca()
@@ -168,7 +172,7 @@ def plot_regen_brake_effect(file_lists, folder_path):
 
         # Use the masked data for the plot
         ax1.plot(t_min_masked, data_energy_masked, label='BMS Power (kW)', color='tab:red')
-        ax1.plot(t_min_masked, D_masked.cumsum(), label='D Power (kW)', color='tab:blue')
+        ax1.plot(t_min_masked, D_masked, label='D term Power (kW)', color='tab:blue')
 
         ax2 = ax1.twinx()  # Create a second y-axis
         ax2.set_ylabel('Acceleration')  # Add label to the second y-axis
@@ -187,4 +191,97 @@ def plot_regen_brake_effect(file_lists, folder_path):
 
         plt.title('BMS Data and D Power with Acceleration')
         plt.tight_layout()
+        plt.show()
+
+def plot_power_comparison(file_lists, folder_path):
+    for file in tqdm(file_lists[31:35]):
+        file_path = os.path.join(folder_path, file)
+        data = pd.read_csv(file_path)
+
+        t = pd.to_datetime(data['time'], format='%Y-%m-%d %H:%M:%S')
+        t_diff = t.diff().dt.total_seconds().fillna(0)
+        t_diff = np.array(t_diff.fillna(0))
+        t_min = (t - t.iloc[0]).dt.total_seconds() / 60  # Convert time difference to minutes
+
+        bms_power = data['Power_IV'] / 1000
+        model_power = data['Power'] / 1000
+        A_power = data['A'] / 1000
+        B_power = data['B'] / 1000
+        C_power = data['C'] / 1000
+        D_power = data['D'] / 1000
+        E_power = data['E'] / 1000
+
+        # Plot the comparison graph
+        plt.figure(figsize=(10, 6))  # Set the size of the graph
+        plt.xlabel('Time (minutes)')
+        plt.ylabel('BMS Power and Model Power (kW)')
+        plt.plot(t_min, bms_power, label='BMS Power (kW)', color='tab:blue')
+        plt.plot(t_min, model_power, label='model Power (kW)', color='tab:red')
+        # plt.plot(t_min, A_power, label='v Term (kW)', color='tab:orange')
+        # plt.plot(t_min, B_power, label='v^2 Term (kW)', color='tab:purple')
+        # plt.plot(t_min, C_power, label='v^3 Term (kW)', color='tab:pink')
+        # plt.plot(t_min, D_power, label='Acceleration Term (kW)', color='tab:green')
+        # plt.plot(t_min, E_power, label='Aux/Idle Term (kW)', color='tab:brown')
+
+        # Add date and file name
+        date = t.iloc[0].strftime('%Y-%m-%d')
+        plt.text(1, 1, date, transform=plt.gca().transAxes, fontsize=12,
+                 verticalalignment='top', horizontalalignment='right', color='black')
+        plt.text(0, 1, 'File: '+file, transform=plt.gca().transAxes, fontsize=12,
+                 verticalalignment='top', horizontalalignment='left', color='black')
+
+        plt.legend(loc='upper left', bbox_to_anchor=(0, 0.97))
+        plt.title('Model Power vs. BMS Power')
+        plt.tight_layout()
+        plt.show()
+
+def plot_power_diff(file_lists, folder_path):
+    for file in tqdm(file_lists[31:35]):
+        file_path = os.path.join(folder_path, file)
+        data = pd.read_csv(file_path)
+
+        t = pd.to_datetime(data['time'], format='%Y-%m-%d %H:%M:%S')
+        t_diff = t.diff().dt.total_seconds().fillna(0)
+        t_diff = np.array(t_diff.fillna(0))
+        t_min = (t - t.iloc[0]).dt.total_seconds() / 60  # Convert time difference to minutes
+
+        energy_diff = (data['Power_IV'] - data['Power']) / 1000
+
+        # Plot the comparison graph
+        plt.figure(figsize=(10, 6))  # Set the size of the graph
+        plt.xlabel('Time (minutes)')
+        plt.ylabel('BMS Power - Model Power (kW)')
+        plt.plot(t_min, energy_diff, label='BMS Power - Model Power (kW)', color='tab:blue')
+
+        # Add date and file name
+        date = t.iloc[0].strftime('%Y-%m-%d')
+        plt.text(1, 1, date, transform=plt.gca().transAxes, fontsize=12,
+                 verticalalignment='top', horizontalalignment='right', color='black')
+        plt.text(0, 1, 'File: '+file, transform=plt.gca().transAxes, fontsize=12,
+                 verticalalignment='top', horizontalalignment='left', color='black')
+
+        plt.legend(loc='upper left', bbox_to_anchor=(0, 0.97))
+        plt.title('BMS Power & Model Power Difference')
+        plt.tight_layout()
+        plt.show()
+
+def plot_correlation(file_lists, folder_path):
+    for file in tqdm(file_lists[31:35]):
+        file_path = os.path.join(folder_path, file)
+        data = pd.read_csv(file_path)
+
+        t = pd.to_datetime(data['time'], format='%Y-%m-%d %H:%M:%S')
+        t_diff = t.diff().dt.total_seconds().fillna(0)
+        t_diff = np.array(t_diff.fillna(0))
+        t_min = (t - t.iloc[0]).dt.total_seconds() / 60  # Convert time difference to minutes
+
+        data['energy_diff'] = (data['Power_IV'] - data['Power']) / 1000
+
+        # Compute the correlation between energy_diff and each of A_power, B_power, C_power, D_power, E_power
+        correlations = data[['energy_diff', 'A', 'B', 'C', 'D', 'E']].corr()
+
+        # Create a heatmap
+        plt.figure(figsize=(10, 6))
+        sns.heatmap(correlations, annot=True, cmap='coolwarm', center=0)
+        plt.title('Correlation Matrix of energy_diff and Power Terms')
         plt.show()
