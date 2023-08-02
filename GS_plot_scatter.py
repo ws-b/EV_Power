15d5d22a71101faae134643499050d8fa910369e
ws-b <mu4215@gmail.com>
2023-08-02 14:41:20 +0900

    Update GS_plot_scatter.py

--- GS_plot_scatter.py
+++ GS_plot_scatter.py
@@ -104,3 +104,89 @@ def plot_scatter_tbt(file_lists, folder_path):
 
         plt.title('BMS Energy vs. Model Energy')
         plt.show()
+def plot_temp_energy(file_lists, folder_path):
+    all_distance_per_total_energy = []
+    ext_temp_avg = []
+
+    for file in tqdm(file_lists):
+        file_path = os.path.join(folder_path, file)
+        data = pd.read_csv(file_path)
+
+        # Get the average of the ext_temp column
+        ext_temp_mean = data['ext_temp'].mean()
+        ext_temp_avg.append(ext_temp_mean)
+
+        v = data['speed']
+        v = np.array(v)
+
+        t = pd.to_datetime(data['time'], format='%Y-%m-%d %H:%M:%S')
+        t_diff = t.diff().dt.total_seconds().fillna(0)
+        t_diff = np.array(t_diff.fillna(0))
+
+        bms_power = data['Power_IV']
+        bms_power = np.array(bms_power)
+        data_energy = bms_power * t_diff / 3600 / 1000
+        data_energy_cumulative = data_energy.cumsum()
+
+        distance = v * t_diff
+        total_distance = distance.cumsum()
+
+        distance_per_total_energy = (total_distance[-1] / 1000) / data_energy_cumulative[-1] if data_energy_cumulative[-1] != 0 else 0
+        all_distance_per_total_energy.append(distance_per_total_energy)
+
+    # plot the graph
+    fig, ax = plt.subplots(figsize=(8, 6))  # set the size of the graph
+
+    ax.set_xlabel('Average External Temperature')
+    ax.set_ylabel('BMS Mileage (km/kWh)')
+
+    # Scatter plot
+    ax.scatter(ext_temp_avg, all_distance_per_total_energy, c='b')
+
+    # Add trendline
+    slope, intercept, _, _, _ = linregress(ext_temp_avg, all_distance_per_total_energy)
+    ax.plot(ext_temp_avg, intercept + slope * np.array(ext_temp_avg), 'r')
+
+    plt.ylim(0, 15)
+    plt.title("Average External Temperature vs. BMS Energy")
+    plt.show()
+
+def plot_distance_energy(file_lists, folder_path):
+    all_distance_per_total_energy = []
+    all_distance = []
+
+    for file in tqdm(file_lists):
+        file_path = os.path.join(folder_path, file)
+        data = pd.read_csv(file_path)
+
+        v = data['speed']
+        v = np.array(v)
+
+        t = pd.to_datetime(data['time'], format='%Y-%m-%d %H:%M:%S')
+        t_diff = t.diff().dt.total_seconds().fillna(0)
+        t_diff = np.array(t_diff.fillna(0))
+
+        bms_power = data['Power_IV']
+        bms_power = np.array(bms_power)
+        data_energy = bms_power * t_diff / 3600 / 1000
+        data_energy_cumulative = data_energy.cumsum()
+
+        distance = v * t_diff
+        total_distance = distance.cumsum()
+
+        distance_per_total_energy = (total_distance[-1] / 1000) / data_energy_cumulative[-1] if data_energy_cumulative[-1] != 0 else 0
+        all_distance_per_total_energy.append(distance_per_total_energy)
+        all_distance.append(total_distance[-1] / 1000)
+
+    # plot the graph
+    fig, ax = plt.subplots(figsize=(8, 6))  # set the size of the graph
+
+    ax.set_xlabel('Distance (km)')
+    ax.set_ylabel('BMS Mileage (km/kWh)')
+
+    # Scatter plot
+    ax.scatter(all_distance, all_distance_per_total_energy, c='b')
+    plt.xlim(0, 100)
+    plt.ylim(3, 10)
+    plt.title("Distance vs. BMS Energy")
+    plt.show()
