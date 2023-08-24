0cf6d4ae124ede0cfbfb61be3d9d5cd6fafe5fbb
ws-b <mu4215@gmail.com>
2023-08-24 12:57:36 +0900

    Update GS_Model_Main.py

--- GS_Model_Main.py
+++ GS_Model_Main.py
@@ -37,31 +37,35 @@ def main():
     print("4: Quitting the program.")
     car = int(input("Select Car you want to calculate: "))
 
-    if car == 1:
+    folder_path = os.path.normpath('D:\\Data\\대학교 자료\\켄텍 자료\\삼성미래과제\\한국에너지공과대학교_샘플데이터')
+    folder_path = os.path.join(folder_path, 'trip_by_trip')
+
+    if car == 1: #ioniq5
         EV = select_vehicle(car)
-        vehicle_name = 'ioniq5'
-    elif car == 2:
+        all_file_lists = get_file_list(folder_path)
+        file_lists = [file for file in all_file_lists if '01241248782' in file]
+    elif car == 2: #kona_ev
         EV = select_vehicle(car)
-        vehicle_name = 'kona_ev'
-    elif car == 3:
+        all_file_lists = get_file_list(folder_path)
+        file_lists = [file for file in all_file_lists if '01241248726' in file]
+    elif car == 3: #porter_ev
         EV = select_vehicle(car)
-        vehicle_name = 'porter_ev'
+        all_file_lists = get_file_list(folder_path)
+        file_lists = [file for file in all_file_lists if '01241228177' in file]
     elif car == 4:
         print("Quitting the program.")
         return
     else:
         print("Invalid choice. Please try again.")
 
-    folder_path = os.path.normpath('D:\\Data\\대학교 자료\\켄텍 자료\\삼성미래과제\\한국에너지공과대학교_샘플데이터')
-    folder_path = os.path.join(folder_path, vehicle_name)
-    file_lists = get_file_list(folder_path)
+    file_lists.sort()
 
     while True:
         print("1: Calculate Power(W) using Model & Filtering Data")
-        print("2: Plotting Energy Graph(Scatter, Line)")
+        print("2: Fitting Model")
         print("3: Plotting Energy Distribution")
         print("4: Plotting All Graph")
-        print("5: Fitting Model")
+        print("5: Plotting Energy Graph(Scatter, Line)")
         print("6: Quitting the program.")
         choice = int(input("Enter number you want to run: "))
 
@@ -69,6 +73,76 @@ def main():
             process_files_power(file_lists, folder_path, EV)
             break
         elif choice == 2:
+            while True:
+                print("1: Fitting Model")
+                print("2: Plotting Energy Distribution")
+                print("3: Plotting Energy/Power Comparison Graph")
+                print("4: Plotting Scatter Graph")
+                print("5: Plotting contour Graph ")
+                print("6: Plotting ")
+                print("7: Plotting ")
+                print("8: Plotting ")
+                print("9: Quitting the program.")
+                choice = int(input("Enter number you want to run: "))
+
+                if choice == 1:
+                    fitting(file_lists, folder_path)
+                    break
+                elif choice == 2:
+                    plot_fit_model_energy_dis(file_lists, folder_path)
+                    break
+                elif choice == 3:
+                    plot_fit_energy_comparison(file_lists, folder_path)
+                    plot_fit_power_comparison(file_lists, folder_path)
+                    break
+                elif choice == 4:
+                    plot_fit_scatter_all_trip(file_lists, folder_path)
+                    plot_fit_scatter_tbt(file_lists, folder_path)
+                    break
+                elif choice == 5:
+                    break
+                elif choice == 6:
+                    break
+                elif choice == 7:
+                    break
+                elif choice == 8:
+                    break
+                elif choice == 9:
+                    print("Quitting the program.")
+                    return
+                else:
+                    print("Invalid choice. Please try again.")
+
+            break
+        elif choice == 3:
+            while True:
+                print("1: Plotting Model's Energy Distribution")
+                print("2: Plotting BMS's Energy Distribution")
+                print("3: Plotting All graph")
+                print("4: Quitting the program.")
+                plot = int(input("Enter number you want to run: "))
+                if plot == 1:
+                    plot_model_energy_dis(file_lists, folder_path)
+                    break
+                elif plot == 2:
+                    plot_bms_energy_dis(file_lists, folder_path)
+                    break
+                elif plot == 3:
+                    plot_model_energy_dis(file_lists, folder_path)
+                    plot_bms_energy_dis(file_lists, folder_path)
+                    break
+                elif plot == 4:
+                    print("Quitting the program.")
+                    return
+            break
+        elif choice == 4:
+            plot_scatter_all_trip(file_lists, folder_path)
+            plot_scatter_tbt(file_lists, folder_path)
+            plot_energy_comparison(file_lists, folder_path)
+            plot_model_energy_dis(file_lists, folder_path)
+            plot_bms_energy_dis(file_lists, folder_path)
+            break
+        elif choice == 5:
             while True:
                 print("1: Plotting Power Plot Term by Term")
                 print("2: Plotting each trip's Energy Graph(Line)")
@@ -160,78 +234,6 @@ def main():
                     return
                 else:
                     print("Invalid choice. Please try again.")
-            break
-        elif choice == 3:
-            while True:
-                print("1: Plotting Model's Energy Distribution")
-                print("2: Plotting BMS's Energy Distribution")
-                print("3: Plotting All graph")
-                print("4: Quitting the program.")
-                plot = int(input("Enter number you want to run: "))
-                if plot == 1:
-                    plot_model_energy_dis(file_lists, folder_path)
-                    break
-                elif plot == 2:
-                    plot_bms_energy_dis(file_lists, folder_path)
-                    break
-                elif plot == 3:
-                    plot_model_energy_dis(file_lists, folder_path)
-                    plot_bms_energy_dis(file_lists, folder_path)
-                    break
-                elif plot == 4:
-                    print("Quitting the program.")
-                    return
-            break
-        elif choice == 4:
-            plot_scatter_all_trip(file_lists, folder_path)
-            plot_scatter_tbt(file_lists, folder_path)
-            plot_energy_comparison(file_lists, folder_path)
-            plot_model_energy_dis(file_lists, folder_path)
-            plot_bms_energy_dis(file_lists, folder_path)
-            break
-        elif choice == 5:
-            while True:
-                print("1: Fitting Model")
-                print("2: Plotting Energy Distribution")
-                print("3: Plotting Energy/Power Comparison Graph")
-                print("4: Plotting Scatter Graph")
-                print("5: Plotting contour Graph ")
-                print("6: Plotting ")
-                print("7: Plotting ")
-                print("8: Plotting ")
-                print("9: Quitting the program.")
-                choice = int(input("Enter number you want to run: "))
-
-                if choice == 1:
-                    folder_path = r'D:\Data\대학교 자료\켄텍 자료\삼성미래과제\한국에너지공과대학교_샘플데이터\merged'
-                    file_lists = get_file_list(folder_path)
-                    fitting(file_lists, folder_path)
-                    break
-                elif choice == 2:
-                    plot_fit_model_energy_dis(file_lists, folder_path)
-                    break
-                elif choice == 3:
-                    plot_fit_energy_comparison(file_lists, folder_path)
-                    plot_fit_power_comparison(file_lists, folder_path)
-                    break
-                elif choice == 4:
-                    plot_fit_scatter_all_trip(file_lists, folder_path)
-                    plot_fit_scatter_tbt(file_lists, folder_path)
-                    break
-                elif choice == 5:
-                    break
-                elif choice == 6:
-                    break
-                elif choice == 7:
-                    break
-                elif choice == 8:
-                    break
-                elif choice == 9:
-                    print("Quitting the program.")
-                    return
-                else:
-                    print("Invalid choice. Please try again.")
-
             break
         elif choice == 6:
             print("Quitting the program.")
