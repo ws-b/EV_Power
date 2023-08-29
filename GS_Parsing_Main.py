c056c61dbad990d23c4ecb855d1bedab9586973f
ws-b <mu4215@gmail.com>
2023-08-29 10:50:26 +0900

    Update GS_Parsing_Main.py

--- GS_Parsing_Main.py
+++ GS_Parsing_Main.py
@@ -11,8 +11,8 @@ def pre_process():
         choice = int(input("Enter number you want to run: "))
 
         if choice == 1:
-            folder_path = os.path.normpath('D:\\Data\\대학교 자료\\켄텍 자료\\삼성미래과제\\한국에너지공과대학교_샘플데이터')
-            save_path = os.path.normpath('D:\\Data\\대학교 자료\\켄텍 자료\\삼성미래과제\\한국에너지공과대학교_샘플데이터\\processed')
+            folder_path = os.path.normpath(r'D:\Data\대학교 자료\켄텍 자료\삼성미래과제\한국에너지공과대학교_샘플데이터')
+            save_path = os.path.normpath(r'D:\Data\대학교 자료\켄텍 자료\삼성미래과제\한국에너지공과대학교_샘플데이터\processed')
             file_lists = get_file_list(folder_path)
             parse_spacebar(file_lists, folder_path, save_path)
             break
@@ -29,6 +29,13 @@ def pre_process():
             save_path = os.path.normpath(r'D:\Data\대학교 자료\켄텍 자료\삼성미래과제\한국에너지공과대학교_샘플데이터\trip_by_trip')
             file_list = get_file_list(folder_path)
             process_files_trip_by_trip(file_list, folder_path, save_path)
+            break
+
+        elif choice == 4:
+            folder_path = os.path.normpath(r'D:\Data\대학교 자료\켄텍 자료\삼성미래과제\한국에너지공과대학교_샘플데이터\trip_by_trip')
+            save_path = os.path.normpath(r'D:\Data\대학교 자료\켄텍 자료\삼성미래과제\한국에너지공과대학교_샘플데이터\merged')
+            file_list = get_file_list(folder_path)
+            merge_csv_files()
             break
 
         elif choice == 6:
