681dfc5c9fa31ab9d3d282246d80f7393bb020c3
ws-b <mu4215@gmail.com>
2023-09-07 13:33:03 +0900

    Update GS_residual_analysis.py

--- GS_residual_analysis.py
+++ GS_residual_analysis.py
@@ -11,11 +11,11 @@ def select_vehicle(num):
     vehicles = {1: 'ioniq 5', 2: 'kona EV', 3: 'porter EV'}
     return vehicles.get(num, 'unknown')
 
-
 if platform.system() == "Windows":
     folder_path = os.path.normpath(r'D:\Data\대학교 자료\켄텍 자료\삼성미래과제\한국에너지공과대학교_샘플데이터\trip_by_trip')
 elif platform.system() == "Darwin":
-    folder_path = os.path.normpath('/Users/wsong/Documents/KENTECH/삼성미래과제/한국에너지공과대학교_샘플데이터/trip_by_trip')
+    folder_path = os.path.normpath(
+        '/Users/wsong/Documents/KENTECH/삼성미래과제/한국에너지공과대학교_샘플데이터/trip_by_trip')
 else:
     print("Unknown system.")
 
@@ -37,42 +37,51 @@ for file in file_lists:
 save_folder = r'D:\Data\대학교 자료\켄텍 자료\삼성미래과제\한국에너지공과대학교_샘플데이터'
 merged_dataframes = {}
 
+# Process the data
 for key, files in tqdm(grouped_files.items()):
     list_of_dfs = [pd.read_csv(os.path.join(folder_path, f)) for f in files]
     merged_df = pd.concat(list_of_dfs, ignore_index=True)
+    merged_df['Residual'] = (merged_df['Power_IV'] - merged_df['Power']) / abs(merged_df['Power_IV']).mean()
     merged_dataframes[key] = merged_df
-    merged_df['Residual'] = merged_df['Power'] - merged_df['Power_IV']
-    vehicle_type = vehicle_types.get(key, 'unknown')
 
-    # Save merged dataframe to CSV
-    merged_df.sort_values(by='time', ignore_index=True)
-    # merged_df.to_csv(os.path.join(save_folder, f"{key}.csv"), index=False)
+# Plotting
+for key, df in merged_dataframes.items():
+    plt.figure(figsize=(10, 7))
+    sns.set_style("whitegrid")
+    sns.kdeplot(df['Residual'], label=vehicle_types.get(key, 'unknown'), fill=True)
 
-    plt.figure(figsize=(10, 6))
-    sns.regplot(data=merged_df, x='speed', y='Residual', scatter_kws={'alpha':0.5}, line_kws={'color':'red'})
-    plt.title('Relationship between Speed and Residuals')
-    plt.xlabel('Speed (mps)')
-    plt.ylabel('Residual (Power - Power_IV)')
-    plt.text(0.95, 0.95, f'{vehicle_type}', ha='right', va='top', transform=plt.gca().transAxes)
+    plt.xlabel('Residual')
+    plt.ylabel('Density')
+    plt.title(f'Density Plot of Residuals for {vehicle_types.get(key, "unknown")}')
+    plt.legend()
     plt.grid(True)
     plt.show()
 
-    plt.figure(figsize=(10, 6))
-    sns.regplot(data=merged_df, x='acceleration', y='Residual', scatter_kws={'alpha':0.5}, line_kws={'color':'red'})
-    plt.title('Relationship between Acceleration and Residuals')
-    plt.xlabel('Acceleration (mps^2)')
-    plt.ylabel('Residual (Power - Power_IV)')
-    plt.text(0.95, 0.95, f'{vehicle_type}', ha='right', va='top', transform=plt.gca().transAxes)
-    plt.grid(True)
-    plt.show()
-
-    plt.figure(figsize=(10, 6))
-    sns.regplot(data=merged_df, x='ext_temp', y='Residual', scatter_kws={'alpha':0.5}, line_kws={'color':'red'})
-    plt.title('Relationship between External Temperature and Residuals')
-    plt.xlabel('External Temperature (°C)')
-    plt.ylabel('Residual (Power - Power_IV)')
-    plt.text(0.95, 0.95, f'{vehicle_type}', ha='right', va='top', transform=plt.gca().transAxes)
-    plt.grid(True)
-    plt.show()
+    # plt.figure(figsize=(10, 6))
+    # sns.regplot(data=merged_df, x='speed', y='Residual', scatter_kws={'alpha':0.5}, line_kws={'color':'red'})
+    # plt.title('Relationship between Speed and Residuals')
+    # plt.xlabel('Speed (mps)')
+    # plt.ylabel('Residual (Power - Power_IV)')
+    # plt.text(0.95, 0.95, f'{vehicle_type}', ha='right', va='top', transform=plt.gca().transAxes)
+    # plt.grid(True)
+    # plt.show()
+    #
+    # plt.figure(figsize=(10, 6))
+    # sns.regplot(data=merged_df, x='acceleration', y='Residual', scatter_kws={'alpha':0.5}, line_kws={'color':'red'})
+    # plt.title('Relationship between Acceleration and Residuals')
+    # plt.xlabel('Acceleration (mps^2)')
+    # plt.ylabel('Residual (Power - Power_IV)')
+    # plt.text(0.95, 0.95, f'{vehicle_type}', ha='right', va='top', transform=plt.gca().transAxes)
+    # plt.grid(True)
+    # plt.show()
+    #
+    # plt.figure(figsize=(10, 6))
+    # sns.regplot(data=merged_df, x='ext_temp', y='Residual', scatter_kws={'alpha':0.5}, line_kws={'color':'red'})
+    # plt.title('Relationship between External Temperature and Residuals')
+    # plt.xlabel('External Temperature (°C)')
+    # plt.ylabel('Residual (Power - Power_IV)')
+    # plt.text(0.95, 0.95, f'{vehicle_type}', ha='right', va='top', transform=plt.gca().transAxes)
+    # plt.grid(True)
+    # plt.show()
 
 print("Done")
