cbcf3d987def377d6b4f82110acf96c4c8da04b5
ws-b <mu4215@gmail.com>
2023-10-23 17:41:58 +0900

    Update GS_Moving_Average_Residual.py

--- GS_Moving_Average_Residual.py
+++ GS_Moving_Average_Residual.py
@@ -5,13 +5,13 @@ from collections import defaultdict
 import os
 
 # 주어진 디렉토리에서 파일 목록을 가져오기
-directory_path = '/Users/wsong/Documents/삼성미래과제/한국에너지공과대학교_샘플데이터/trip_by_trip'  # 사용자의 디렉토리 경로를 입력하세요
+directory_path = '/Users/wsong/Documents/삼성미래과제/한국에너지공과대학교_샘플데이터/trip_by_trip'
 file_lists = [f for f in os.listdir(directory_path) if f.endswith('.csv')]
 
 # 파일 목록을 그룹화하기
 grouped_files = defaultdict(list)
 for file in file_lists:
-    key = file[:11]  # 파일 이름에서 특정 부분을 기준으로 그룹화 (여기서는 파일명의 처음 11자리)
+    key = file[:11]  # 파일 이름에서 특정 부분을 기준으로 그룹화
     grouped_files[key].append(file)
 
 # 각 그룹별로 처리
@@ -22,7 +22,7 @@ for key, files in grouped_files.items():
     for file in files:
         filepath = os.path.join(directory_path, file)
         df = pd.read_csv(filepath)
-        df['time'] = pd.to_datetime(df['time'])  # 'time' 열을 datetime 객체로 변환
+        df['time'] = pd.to_datetime(df['time'])
         dataframes.append(df)
 
     merged_data = pd.concat(dataframes, ignore_index=True)
