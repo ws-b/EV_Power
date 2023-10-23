2c9f079e6fd1f91b3f8d305f7d281eaf0e6c993d
ws-b <mu4215@gmail.com>
2023-10-23 13:54:15 +0900

    Update GS_Moving_Average_Residual.py

--- GS_Moving_Average_Residual.py
+++ GS_Moving_Average_Residual.py
@@ -11,49 +11,45 @@ file_lists = [f for f in os.listdir(directory_path) if f.endswith('.csv')]
 # 파일 목록을 그룹화하기
 grouped_files = defaultdict(list)
 for file in file_lists:
-    key = file[:11]
+    key = file[:11]  # 파일 이름에서 특정 부분을 기준으로 그룹화 (여기서는 파일명의 처음 11자리)
     grouped_files[key].append(file)
 
 # 각 그룹별로 처리
 for key, files in grouped_files.items():
     dataframes = []
-    differences = []  # Power와 Power_IV의 차이를 저장할 리스트
-    differences_MA_10s = []  # 10초 이동 평균
-    differences_MA_1min = []  # 1분 이동 평균
-    differences_MA_5min = []  # 5분 이동 평균
 
     # 각 그룹별로 파일을 병합하기
     for file in files:
         filepath = os.path.join(directory_path, file)
         df = pd.read_csv(filepath)
-        df['time'] = pd.to_datetime(df['time'])
+        df['time'] = pd.to_datetime(df['time'])  # 'time' 열을 datetime 객체로 변환
         dataframes.append(df)
 
     merged_data = pd.concat(dataframes, ignore_index=True)
-    merged_data = merged_data.sort_values(by='time').reset_index(drop=True)
+    merged_data = merged_data.sort_values(by='time').reset_index(drop=True)  # 시간 순으로 데이터 정렬
 
     # 병합된 데이터에서 Power와 Power_IV의 차이 계산하기
     merged_data['Power_Difference'] = merged_data['Power'] - merged_data['Power_IV']
 
-    # Power_Difference와 그에 대한 이동 평균 계산하기
-    merged_data['Power_Diff_MA_10s'] = merged_data['Power_Difference'].rolling(window=5).mean()
-    merged_data['Power_Diff_MA_1min'] = merged_data['Power_Difference'].rolling(window=30).mean()
-    merged_data['Power_Diff_MA_5min'] = merged_data['Power_Difference'].rolling(window=150).mean()
+    # Residual 값을 정규화하기 위해 절대값의 평균으로 나누기
+    absolute_mean = merged_data['Power_Difference'].abs().mean()  # 절대값의 평균 계산
+    merged_data['Normalized_Power_Difference'] = merged_data['Power_Difference'] / absolute_mean  # 정규화
 
-    differences.extend(merged_data['Power_Difference'].tolist())
-    differences_MA_10s.extend(merged_data['Power_Diff_MA_10s'].tolist())
-    differences_MA_1min.extend(merged_data['Power_Diff_MA_1min'].tolist())
-    differences_MA_5min.extend(merged_data['Power_Diff_MA_5min'].tolist())
+    # Power_Difference와 그에 대한 이동 평균 계산하기
+    merged_data['Power_Diff_MA_10s'] = merged_data['Normalized_Power_Difference'].rolling(window=5).mean()
+    merged_data['Power_Diff_MA_1min'] = merged_data['Normalized_Power_Difference'].rolling(window=30).mean()
+    merged_data['Power_Diff_MA_5min'] = merged_data['Normalized_Power_Difference'].rolling(window=150).mean()
 
     # 각 그룹별 Power_Difference의 분포와 이동 평균 분포를 시각화하기
     plt.figure(figsize=(10, 7))
-    sns.kdeplot(differences, label='Power Difference')
-    sns.kdeplot(differences_MA_10s, label='10s MA')
-    sns.kdeplot(differences_MA_1min, label='1min MA')
-    sns.kdeplot(differences_MA_5min, label='5min MA')
-    plt.title(f'Distribution of Power Differences and its Moving Averages for {key}')
-    plt.xlabel('Power Difference (Power - Power_IV)')
+    sns.kdeplot(merged_data['Normalized_Power_Difference'], label='Normalized Power Difference (2s)')
+    sns.kdeplot(merged_data['Power_Diff_MA_10s'], label='10s MA')
+    sns.kdeplot(merged_data['Power_Diff_MA_1min'], label='1min MA')
+    sns.kdeplot(merged_data['Power_Diff_MA_5min'], label='5min MA')
+    plt.title(f'Distribution of Normalized Power Differences and its Moving Averages for {key}')
+    plt.xlabel('Normalized Power Difference (Power - Power_IV)')
     plt.ylabel('Density')
     plt.grid(True, which="both", ls="--", c='0.7')
+    plt.xlim(0,1)
     plt.legend()
     plt.show()
